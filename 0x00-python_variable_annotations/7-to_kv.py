485cd518eefc84afadf5fbe77cddf07cebdc11d1
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-01 19:23:53 +0000

    to_kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,6 +7,7 @@ as arguments and returns a tuple.
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple where the first element is a string
