938ee18fd1830b17ab15623c2fdd088f5ec129d5
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-01 20:35:14 +0000

    checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,17 +5,17 @@ Module providing a function to zoom (duplicate) elements in a list.
 
 from typing import List, Union
 
-def zoom_array(lst: List[Union[int, float]], factor: int = 2) -> List[Union[int, float]]:
+def zoom_array(lst: List[Union[int, float]],
+               factor: int = 2) -> List[Union[int, float]]:
     """
     Zoom (duplicate) elements in the list based on the given factor.
 
     Args:
         lst (List[Union[int, float]]): A list of integers or floats to be zoomed.
-        factor (int, optional): The factor by which to duplicate
-        the elements. Defaults to 2.
+        factor (int, optional): The factor by which to duplicate the elements. Defaults to 2.
 
     Returns:
-        List[Union[int, float]]: A list with each element duplicated `factor` times
+        List[Union[int, float]]: A list with each element duplicated `factor` times.
     """
     zoomed_in: List[Union[int, float]] = [
         item for item in lst
