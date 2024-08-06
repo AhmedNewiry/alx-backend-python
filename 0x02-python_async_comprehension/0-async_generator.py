05e3d885bcd42909e1c885f02a7273ff83611350
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-06 21:44:07 +0000

    measure runtiome

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ This module defines an asynchronous generator coroutine.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None]:
     """
     Coroutine that asynchronously generates random numbers.
     This coroutine loops 10 times, each time
