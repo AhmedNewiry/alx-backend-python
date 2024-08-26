5f3d2f25c0b72890c41907d25db233363dc391e6
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-26 17:45:18 +0000

    test requests.get

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ Unit tests for the access_nested_map function from the utils module.
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map  # Assuming utils is the module where the function is defined
+from utils import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     """
