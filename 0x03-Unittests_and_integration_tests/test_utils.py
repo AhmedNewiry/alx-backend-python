e5ed40c74696c0cc675f22d3424122497e7776c2
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-26 17:06:47 +0000

    context manager

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -33,6 +33,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test that access_nested_map raises a KeyError with the expected
+        message when the key is not found in the nested map.
+
+        Args:
+            nested_map (Mapping): The nested dictionary to access.
+            path (Sequence): The path of keys to follow in the nested dictionary.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        # Check that the exception message is as expected
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
 
 if __name__ == "__main__":
     unittest.main()
