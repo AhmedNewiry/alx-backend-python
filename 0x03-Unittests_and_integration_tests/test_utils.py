683e91d1f0dee2708e445fce1d0209093644ce90
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-26 23:43:34 +0000

    fix more

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ Unit tests for the access_nested_map function from the utils module.
 """
 
 import unittest
-from unittest.mock import patch, Mock, MagickMock
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
