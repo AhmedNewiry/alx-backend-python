37708d3539d45becb4757ad1b7858dcb680a73da
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-06-15 09:52:24 +0300

    Registers User, Message, Notification for admin access

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Message, Notification
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'user_id')
+    search_fields = ('username', 'email')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('message_id', 'sender', 'receiver', 'content', 'timestamp')
+    search_fields = ('content',)
+    list_filter = ('timestamp',)
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('notification_id', 'user', 'message', 'created_at', 'is_read')
+    list_filter = ('is_read', 'created_at')
