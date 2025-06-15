eb116abdbf252f956348c6a1bf64c6e9afadb26d
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-06-15 11:31:59 +0300

    Update views for threaded conversations

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import User, Message, MessageHistory
+from .models import  Message, MessageHistory
+from chats.models import User
 from .serializers import  MessageSerializer, MessageHistorySerializer
 from ..chats.serializers import UserSerializer
 
@@ -17,7 +18,7 @@ def delete_user(request):
     return Response({"detail": f"User {username} deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
+    queryset = Message.objects.select_related('sender', 'receiver', 'edited_by', 'parent_message').prefetch_related('replies')
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
 
@@ -28,11 +29,22 @@ class MessageViewSet(viewsets.ModelViewSet):
             sender=user
         ) | Message.objects.filter(
             receiver=user
-        )
+        ).select_related('sender', 'receiver', 'edited_by', 'parent_message').prefetch_related('replies')
+
+    def perform_create(self, serializer):
+        """Set sender to current user."""
+        serializer.save(sender=self.request.user)
 
     def perform_update(self, serializer):
         """Set edited_by to current user on update."""
         serializer.save(edited_by=self.request.user)
+
+    @action(detail=True, methods=['get'])
+    def thread(self, request, pk=None):
+        """Fetch the message and its threaded replies."""
+        message = self.get_object()
+        serializer = MessageSerializer(message, context={'request': request})
+        return Response(serializer.data)
 
 class MessageHistoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = MessageHistory.objects.all()
