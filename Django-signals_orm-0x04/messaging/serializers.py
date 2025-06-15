582c261336d37b3440d67fe2bc4623a7377efb26
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-06-15 11:32:26 +0300

    Update serializers for parent_message and replies

--- Django-signals_orm-0x04/messaging/serializers.py
+++ Django-signals_orm-0x04/messaging/serializers.py
@@ -6,9 +6,17 @@ class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
     receiver = UserSerializer(read_only=True)
     edited_by = UserSerializer(read_only=True)
+    parent_message = serializers.PrimaryKeyRelatedField(queryset=Message.objects.all(), allow_null=True)
+    replies = serializers.SerializerMethodField()
+
     class Meta:
         model = Message
-        fields = ['message_id', 'sender', 'receiver', 'content', 'timestamp', 'edited', 'edited_by']
+        fields = ['message_id', 'sender', 'receiver', 'content', 'timestamp', 'edited', 'edited_by', 'parent_message', 'replies']
+
+    def get_replies(self, obj):
+        """Recursively serialize replies."""
+        replies = obj.get_thread()
+        return MessageSerializer(replies, many=True, context=self.context).data
 
 class NotificationSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
