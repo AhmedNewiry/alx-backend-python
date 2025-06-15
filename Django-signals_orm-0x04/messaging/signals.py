9dc882c36ccdc224cb0dbd8ebeb492a6beeeae6b
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-06-15 10:05:30 +0300

    Use the pre_save signal to log the old content of a message into a separate MessageHistory model before it’s updated

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Message, Notification
 
@@ -10,3 +10,18 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance,
         )
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    """Log the old content of a message before it is updated."""
+    if instance.pk:
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                MessageHistory.objects.create(
+                    message=instance,
+                    old_content=old_message.content
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
